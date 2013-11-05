10fc1939de51c784d0920d07b0940a0475e6e5b2
ZhangZhenli <ZhangZhenli@localhost>
2013-11-05 18:36:35 +0800

    ????

--- python/test.py
+++ python/test.py
@@ -11,7 +11,9 @@ import sys
 from win32gui import *
 from python.keyinput.action_key     import Key
 from python.keyinput.action_text    import Text
-from python.keyinput.window_mgr    import WindowMgr
+from python.keyinput.window_mgr     import WindowMgr
+
+distApkDir = "D:\Android\Deapk\dist\FirefoxV24.0"
 
 def execCmd(cmd):
     print "执行命令:"+cmd
@@ -24,19 +26,32 @@ def DeApk():
     jdcmd = "lib\jd-gui-0.3.5\jd-gui.exe \"D:\Android\Deapk\dist\FirefoxV24.0\classes.jar\""
     subprocess.Popen(jdcmd) # Success!
     WaitAndFore("Java Decompiler")    
-    Key("a-f, s").execute()    
-    WaitAndFore("Save")
-    time.sleep(0.5)
-    action = Key("f4/10:2, c-a, delete") + Text("D:\Android\Deapk\dist\FirefoxV24.0")
-    action.execute()
-    #+ Key("enter/5:4,y")
-    w = WindowMgr()     
-    while w._handle == None:
-        Key("enter/5").execute()
+    Key("a-f, s").execute()   
+    WaitAndFore("Save")    
+    getText()
+    print "准备另存为"+distApkDir+ os.sep+"src.zip"
+    while getText() != "classes.src.zip":
+        WaitAndFore("Save")
+        Key("a-t, a-n, c-a,c-c/2").execute()
+        sys.stdout.write('.')
         time.sleep(0.1)
+    time.sleep(0.1)
+    print ""
+    setText(distApkDir+ os.sep)
+    print "正在另存为"+distApkDir+ os.sep+"src.zip"
+    (Key("a-t, a-n, c-a, delete") + Text("src.zip")).execute()    
+    time.sleep(0.1)
+    Key("a-t, a-d, c-a, delete,c-v,a-s,y").execute()
+    w = WindowMgr()
+    i=50
+    while w._handle == None:
+        #Key("enter").execute()
+        time.sleep(0.1)
+        i = i-1
         w.find_window_wildcard("Save All Sources"+".*")
         sys.stdout.write('.')
     print w._handle
+    print "正在保存源码"
     w = WindowMgr()
     w.find_window_wildcard("Save All Sources"+".*")
     while w._handle != None:
@@ -44,7 +59,18 @@ def DeApk():
         time.sleep(0.01)
         sys.stdout.write('.')
     print ""
-    print "保存完毕"
+    print "保存完毕,正在退出Java Decompiler"
+    w = WindowMgr()
+    w.find_window_wildcard("Java Decompiler"+".*")
+    if w._handle != None:
+        w.set_foreground()
+        Key("a-x").execute()
+    else:
+        print "未发现Java Decompiler"
+    print "正在解压"
+    zipFile = zipfile.ZipFile(distApkDir+ os.sep+"src.zip")
+    zipFile.extractall(distApkDir+ os.sep+"src")
+    zipFile.close()
   #  ListWindows()
     print "执行完毕"
 
@@ -56,6 +82,7 @@ def WaitAndFore(title):
         sys.stdout.write('.')
     print w._handle    
     w.set_foreground()
+    time.sleep(0.1)
 
 titles = set()
 def foo(hwnd,nouse):
@@ -70,6 +97,21 @@ def ListWindows():
     print "---------------------------------------------"
     for t in lt:
         print t
+
+import win32clipboard
+import win32con 
+        
+def getText():  
+    win32clipboard.OpenClipboard()  
+    d = win32clipboard.GetClipboardData(win32con.CF_TEXT)  
+    win32clipboard.CloseClipboard()  
+    return d 
+
+def setText(aString):  
+    win32clipboard.OpenClipboard()  
+    win32clipboard.EmptyClipboard()  
+    win32clipboard.SetClipboardData(win32con.CF_TEXT, aString)  
+    win32clipboard.CloseClipboard()
 
 if __name__ == "__main__":
     DeApk()
