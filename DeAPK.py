32ef0c21a6362a504aafae006c71c145e46728b6
ZhangZhenli <ZhangZhenli@localhost>
2013-11-05 18:36:15 +0800

    ?????,????

--- DeAPK.py
+++ DeAPK.py
@@ -17,8 +17,8 @@ ISOTIMEFORMAT="%Y-%m-%d %X"
 # determine if application is a script file or frozen exe
 if getattr(sys, 'frozen', False):
     currentDir = os.path.dirname(sys.executable)
-elif __file__:
-    currentDir = os.path.dirname(__file__)
+else:
+    currentDir = sys.path[0]
 os.chdir(currentDir)
 apktool_bat =   currentDir+"\\lib\\apktool1.5.1\\apktool.bat"
 dex2jar_bat =   currentDir+"\\lib\\dex2jar-0.0.9.13\\d2j-dex2jar.bat"
@@ -63,33 +63,65 @@ def decompileApk(distdir,apkfilename,logdir):
     for f in text:
         print f
     jdcmd = jd_gui_exe + " "+"\""+distApkDir+ os.sep +"classes.jar"+"\""
-    #text =  execCmd(jdcmd)
-    setText(distApkDir+ os.sep)
     subprocess.Popen(jdcmd) # Success!
     WaitAndFore("Java Decompiler")    
-    Key("a-f, s").execute()    
+    Key("a-f, s").execute()   
     WaitAndFore("Save")
-    time.sleep(0.5)
-    action = Key("f4/10:2, c-a, delete") + Key("c-v/10")
-    action.execute()
-    #+ Key("enter/5:4,y")
-    w = WindowMgr()     
-    while w._handle == None:
-        Key("enter/5").execute()
+    setText("None")
+    print "准备另存为"+distApkDir+ os.sep+"src.zip"
+    while getText() != "classes.src.zip":
+        WaitAndFore("Save")
+        Key("a-t, a-n, c-a,c-c/2").execute()
+        sys.stdout.write('.')
         time.sleep(0.1)
+    time.sleep(0.1)
+    print ""
+    setText(distApkDir+ os.sep)
+    print "正在另存为"+distApkDir+ os.sep+"src.zip"
+    (Key("a-t, a-n, c-a, delete") + Text("src.zip")).execute()    
+    time.sleep(0.1)
+    Key("a-t, a-d, c-a, delete,c-v,a-s,y").execute()
+    w = WindowMgr()
+    i=50
+    while w._handle == None:
+        #Key("enter").execute()
+        time.sleep(0.1)
+        i = i-1
         w.find_window_wildcard("Save All Sources"+".*")
         sys.stdout.write('.')
     print w._handle
+    print "正在保存源码"
     w = WindowMgr()
     w.find_window_wildcard("Save All Sources"+".*")
     while w._handle != None:
         w.find_window_wildcard("Save All Sources"+".*")
-        time.sleep(0.1)
+        time.sleep(0.01)
         sys.stdout.write('.')
     print ""
+    print "保存完毕,正在退出Java Decompiler"
+    w = WindowMgr()
+    w.find_window_wildcard("Java Decompiler"+".*")
+    if w._handle != None:
+        w.set_foreground()
+        Key("a-x").execute()
+    else:
+        print "未发现Java Decompiler"
+    print "正在解压"
+    zipFile = zipfile.ZipFile(distApkDir+ os.sep+"src.zip")
+    #zipFile.extractall(distApkDir+ os.sep+"src")
+    for f in zipFile.namelist():
+        if f.endswith('/'):
+            os.makedirs(distApkDir+ os.sep+"src"+os.sep +f)
+        else:
+            try:
+                zipFile.extract(f,distApkDir+ os.sep+"src")
+            except IOError,e:
+                import traceback
+                traceback.print_exc()           
+    zipFile.close()
     print "保存完毕"
-  #  ListWindows()
-		
+    return jdcmd
+
 def main():
     starttime =  time.strftime( ISOTIMEFORMAT, time.localtime() )
     print("开始时间:"+starttime+"\n")
@@ -109,10 +141,13 @@ def main():
         os.makedirs(logdir)
         i = 0
         length =  len(apks)
+        arr =[]
         for f in apks:
             i = i +1
             print str(i)+"/"+str(length)
-            decompileApk(distdir,f,logdir)
+            arr.append(decompileApk(distdir,f,logdir))
+        for f in arr:
+            subprocess.Popen(f)
         print("结束时间:"+time.strftime( ISOTIMEFORMAT, time.localtime())+"\n")
     else :
         print("没有找到任何APK文件在目录:"+os.path.abspath(currentDir)+ os.sep  + apk_dir +"\n")
